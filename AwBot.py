96fa55de6cbbca96344f5a047b6e6f9b8fea99ae
Oscar Pacheco <oscarpr@mx1.ibm.com>
2017-09-20 23:22:01 -0500

    First commit

---
+++ AwBot.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python
+
+import os
+import time
+import telepot
+import tempfile
+from gtts import gTTS
+from datetime import datetime
+
+
+#This script is meant to be used with the runOnReceive parameter of gammu sms daemon
+
+#Get variables
+BOT_TOKEN = os.environ.get("TEST_BOT") # TEST_BOT env variable should contain the bot id
+USER_ID = os.environ.get("TEST_ID") #TEST_ID env variable should contain the user_id
+USER = os.environ.get("TEST_USER")
+PID = os.getpid() # get PID, currently not used
+SMS_NUMBER = os.environ.get("SMS_1_NUMBER")
+
+t = datetime.now() #get the time
+
+time_format = "{0}{1}{2}_{3}{4}".format(t.year,t.month,t.day,t.hour,t.minute)
+
+numparts = int(os.environ['DECODED_PARTS']) # How many parts we got
+
+text = ''
+# Are there any decoded parts?
+if numparts == 0:
+    text = os.environ['SMS_1_TEXT']
+# Get all text parts
+else:
+    for i in range(0, numparts):
+        varname = 'DECODED_%d_TEXT' % i
+        if varname in os.environ:
+            text = text + os.environ[varname]
+
+# Do something with the text
+full_text="Number {0} have sent text: {1}".format(SMS_NUMBER,text)
+
+bot = telepot.Bot(BOT_TOKEN) #Create bot
+bot.sendMessage(USER_ID, full_text) # send the same text that arrived on the sms
+
+# generate the mp3 message on the fly
+tts = gTTS(text=text, lang="es",slow=False)
+audio_tmp = tempfile.TemporaryFile() #generate a tmp file
+tts.write_to_fp(audio_tmp) # write the audio to the tmp file, use tts.save("disk_file.mp3") to save it to disk
+audio_tmp.seek(0) #need to seek to the beggining of the file to work
+
+audio_name="audio_{0}.mp3".format(time_format)
+bot.sendAudio(USER_ID, (audio_name,audio_tmp)) # send the file
+audio_tmp.close() #Close the tmp file
